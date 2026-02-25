cca933f78f05d32d3e0ef1daa3963cc6228c8dfd
lucas mota <lucasmota408@gmai.com>
2026-02-24 22:14:44 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula 1/src/script.js
@@ -0,0 +1,13 @@
+//Exemplos de variável let
+
+let nome = 'Lucas';
+let nome2 = 'Luiz'
+
+console.log(nome);
+console.log(nome2);
+
+//Exemplos de variável const
+const times = ['Santos', 'SP', 'Bahia'];
+
+
+console.log(times);
